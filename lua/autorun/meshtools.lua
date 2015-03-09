1a7b4ea2096a587474b98298fe54cda71f3c9b57
shadowscion <khaotix2@gmail.com>
2015-03-08 19:19:05 -0500

    shiet

--- lua/autorun/meshtools.lua
+++ lua/autorun/meshtools.lua
@@ -14,6 +14,7 @@ if SERVER then
     AddCSLuaFile( "meshtools/modules/vnet.lua" )
     AddCSLuaFile( "meshtools/modules/decode.lua" )
     AddCSLuaFile( "meshtools/libraries/export.lua" )
+    AddCSLuaFile( "meshtools/libraries/import.lua" )
 
     include( "meshtools/modules/vnet.lua" )
 
@@ -23,9 +24,12 @@ if SERVER then
 end
 
 if CLIENT then
+    meshtools.MeshCache = meshtools.MeshCache or {}
+
     include( "meshtools/modules/vnet.lua" )
     include( "meshtools/modules/decode.lua" )
     include( "meshtools/libraries/export.lua" )
+    include( "meshtools/libraries/import.lua" )
 
     return
 end
